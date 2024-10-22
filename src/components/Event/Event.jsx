d4e57af76092939c732e81985deb42e5078b9b4e
unknown <you@example.com>
2024-10-22 12:32:53 +0200

    fixed all now done

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -6,7 +6,6 @@ function Event(props) {
 
     function handleClick() {
         navigate('/event', { state: { event: event } });
-        fetch(`${import.meta.env.VITE_API_URL}/api/events`);
     }
 
     return (
